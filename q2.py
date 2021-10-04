fb7dd6e7a8f16d800cdc9132cb2fecf05ef168d3
Johnny B <johnnybexplore@gmail.com>
2021-10-04 08:43:33 -0400

    removing prints

--- q2.py
+++ q2.py
@@ -4,36 +4,36 @@ def decryptPassword(s):
     count = len(s)
     numcontainer = []
     offset = 0
-    print('string ', s)
-    #print('count: ',count)
+    # print('string ', s)
+    # #print('count: ',count)
     for key in range(0,count):
-        print('--key: ', key)
-        print('letter: ', s[key])
+        # print('--key: ', key)
+        # print('letter: ', s[key])
 
         if s[key].isnumeric() and s[key] != '0':
             numcontainer.append(s[key])
-            print('append to numcontainer ',numcontainer)
+            # print('append to numcontainer ',numcontainer)
             offset += 1
         elif s[key] == '*':
             pwd[key - 2 - offset] = s[key -1].lower()
             pwd[key - 1 - offset] = s[key -2].upper()
-            print('letter switch ', pwd)
+            # print('letter switch ', pwd)
         elif s[key] == '0' :
             pwd[key - offset] = numcontainer.pop(-1)
-            print('found zero dumping numcontainer ' , pwd)
+            # print('found zero dumping numcontainer ' , pwd)
         else :
             pwd[key - offset] = s[key]
-            print('adding to pwd ', pwd)
-    print('final pwd ',"".join(pwd.values()))
+            # print('adding to pwd ', pwd)
+    # print('final pwd ',"".join(pwd.values()))
     return "".join(pwd.values())
 
 q = '51Pa*0Lp*0e'
 mya = decryptPassword(q)
-print(mya)
+# print(mya)
 a = 'aP1pL5e'
-print('final should be: ',a)
+# print('final should be: ',a)
 q1 = 'pTo*Ta*O'
 mya = decryptPassword(q1)
-print(mya)
+# print(mya)
 a1 = 'poTaTO'
-print('final should be: ',a1)
+# print('final should be: ',a1)
